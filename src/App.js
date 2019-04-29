13db9a6992ac978d9e06523c666b2c613b1a5bd0
fuckJD <>
2019-04-29 14:02:12 +0800

    mark

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
 
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+  </GoogleMap>
+))
 function App() {
   return (
     <div className="App">
