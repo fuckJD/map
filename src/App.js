785f7f28cbe15b31da8b3dc6d8b99783deff4c3d
fuckJD <cays2017cays@gmail.com>
2019-04-30 16:36:22 +0800

    add list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css'
 import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps'
 import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer'
 import axios from 'axios'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) => {
     let points = []
@@ -30,16 +35,7 @@ class App extends Component {
   constructor (e) {
     super(e)
     this.state = {
-      points: [
-        {
-          'autor': '',
-          'describe': '',
-          'location': {
-            'lat': 39.8401204,
-            'lng': 116.5900514
-          }
-        }
-      ]
+      list: []
     }
   }
 
@@ -49,22 +45,51 @@ class App extends Component {
     )
       .then(res => {
         const points = res.data.list
-        console.log(points)
         this.setState({ list: points })
       })
+  }
+
+  renderCardItem (item) {
+    console.log(item)
+    return (
+      <Card style={{ marginBottom: '20px' }}>
+        <CardContent>
+          <Typography component="p" style={{ textAlign: 'left', fontSize: '18px'  }}>
+            事件：<a href={item.link}>{item.describe}</a>
+            <br/>
+          </Typography>
+          <Typography component="p" style={{ textAlign: 'left', fontSize: '14px'  }}>
+            happened: {item.time}
+            <br/>
+          </Typography>
+          <Typography style={{ textAlign: 'right', fontSize: '14px' }} color="textSecondary" gutterBottom>
+            贡献者{item.autor}
+          </Typography>
+        </CardContent>
+      </Card>
+    )
   }
 
   render () {
     return (
       <div className="App">
-        <MyMapComponent
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8f78kK64cbZIt1C23qW5bH0-iUbSi2bM&libraries=visualization"
-          data={this.state.list}
-          loadingElement={<div style={{ height: `100%` }}/>}
-          containerElement={<div style={{ height: `1000px` }}/>}
-          mapElement={<div style={{ height: `100%` }}/>}
-        >
-        </MyMapComponent>
+        <div style={{ width: `74%`, height: `100%` }}>
+          <MyMapComponent
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8f78kK64cbZIt1C23qW5bH0-iUbSi2bM&libraries=visualization"
+            data={this.state.list}
+            loadingElement={<div style={{ height: `100%` }}/>}
+            containerElement={<div style={{ height: `1000px` }}/>}
+            mapElement={<div style={{ height: `100%` }}/>}
+          >
+          </MyMapComponent>
+        </div>
+        <div style={{ width: `30%`, margin: `3%` }}>
+          {
+            this.state.list.map((item) => {
+              return this.renderCardItem(item)
+            })
+          }
+        </div>
       </div>
     )
   }
